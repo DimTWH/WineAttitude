9e5ab5822c8ea6b0b6231699153ded215114d7b9
Dima <dimpc.balan003@outlook.com>
2022-06-16 12:45:34 +0200

    footer mail icon aligned

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -372,7 +372,7 @@
                         <!--<li><i class="fa fa-paper-plane"></i>Bengaluru, Karnataka</li>-->
                         <li>
                             <a href="mailto:mywine.attitude@gmail.com">
-                                <i class="fa fa-envelope"></i>
+                                <i class="fa fa-envelope" style="line-height: 30px;"></i>
                                  mywine.attitude@gmail.com
                             </a>
                         </li>
